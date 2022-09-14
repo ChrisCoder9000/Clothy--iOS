a67ee4cc86765a2b0e11be0f3c132cea4fb143db
ChrisCoder9000 <alch.infoemail@gmail.com>
2022-09-14 13:25:45 +0200

    Initial Commit

---
+++ Clothy/ClothyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothyApp.swift
+//  Clothy
+//
+//  Created by Christian Nonis on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ClothyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
